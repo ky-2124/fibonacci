45ddf874ede5acd93edfba03360eecfe0ff5b2c8
koji <yoneta@techmatrix.co.jp>
2023-07-11 17:04:15 +0900

    メモによるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 'use strict';
-function fib(n) {
-  if(n===0) {
-    return 0;
-  } else if (n===1) {
-    return 1;
+const memo = new Map();  //memoという定数のMapを作成
+memo.set(0, 0);  //0というキーに0という値を設定、キー：順番　値：答え
+memo.set(1, 1);  //1というキーに1という値を設定
+
+function fib(n) {  //nを引数とするfibという名前の関数を定義
+  if (memo.has(n)) {  //もし定数memoがnというキーを保持していれば
+    return memo.get(n);  //定数memoのキーのnを返す？
   }
-  return fib(n-1) + fib(n-2);
+  const value = fib(n - 1) + fib(n - 2);  //定数valueを定義
+  memo.set(n, value);  //nというキーに定数valueを設定
+  return value;  //定数valueを返す
 }
+
 const length = 40;
 for (let i = 0; i <= length; i++) {
-  console.log(fib(i));
-  
+  console.log(fib(i));  //fib()関数をここで使っている。
+
 }
